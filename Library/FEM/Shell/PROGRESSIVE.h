6ea4ce072273cd38e033d4ac36d70a6d20c4758a
HaitaoYu <haitao.yu-nd@disneyresearch.com>
2024-04-25 16:29:16 +0200

    start progressive smocking

--- Library/FEM/Shell/PROGRESSIVE.h
+++ Library/FEM/Shell/PROGRESSIVE.h
@@ -1,9 +1,15 @@
 #pragma once
 
+/* Progressive add smocking patterns,
+support adding irragular smocking patch, 
+potential app scenario: smocking design on a loose draped cloth
+*/
 namespace JGSL{
 
+// Progressive adding stitching according to a stithcing index seq. of full smocking pattern 
 
 
+// progressive updating graph
 
 
 }
